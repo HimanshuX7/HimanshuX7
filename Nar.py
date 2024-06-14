2edb023474a1bd2ea9fbb5295471a4b2739a4eff
HimanshuX7 <150924393+HimanshuX7@users.noreply.github.com>
2024-06-14 09:52:53 +0530

    Update Nar.py

--- Nar.py
+++ Nar.py
@@ -29,7 +29,7 @@ def home():
 
 starting_letter_pattern = r"start with ([A-Z])"
 min_length_pattern = r"include at least (\d+) letters"
-trigger_pattern = r"Turn: ᏻ𝘰ԁ 𝘚𝔥𝞪𝘥𝜊𝗐.*" # Replace "ᖇᗩᕼᑌᒪ" with your own trigger pattern (Your telegram profile name)
+trigger_pattern = r"Turn: GOᗪᔕᕼᗩᗪOᗯ.*" # Replace "ᖇᗩᕼᑌᒪ" with your own trigger pattern (Your telegram profile name)
 
 
 @app.on_message(filters.me & filters.command("ping", prefixes="!"))
